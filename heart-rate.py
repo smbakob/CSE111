5fd5ac4d5ccb45f7cab1914a6d60f2b57b161d9e
smbakob <smbakob1254@gmail.com>
2025-04-27 23:28:08 -0400

    test

--- heart-rate.py
+++
@@ -1,9 +0,0 @@
-name=input("what is your name ")
-age=int(input("what is your age "))
-
-maximum=220-age
-
-slow=maximum* 0.65
-fast=maximum*0.85
-
-print (f"when you excersise you should keep your heart rate between {slow} and {fast}")

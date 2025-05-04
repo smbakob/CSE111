d761ac4b1427276476792633b1cdadbb48eb5265
smbakob <smbakob1254@gmail.com>
2025-05-03 23:21:52 -0400

    mission 2

--- tire_volume.py
+++
@@ -1,8 +0,0 @@
-import math
-
-width=float(input("what is the width of the tire "))
-ar=float(input("what is the aspect ratio "))
-diameter=float(input("what is the diameter "))
-v=(math.pi*(width**2)*ar*(width*ar+2540*diameter)) / 10000000000
-
-print(f"the volume is about {v}")

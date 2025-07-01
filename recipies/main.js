9e737517416141254e988947753fb45726b8c81a
smbakob <smbakob1254@gmail.com>
2025-07-01 12:06:20 -0400

    commit

--- recipies/main.js
+++ recipies/main.js
@@ -1,16 +1,16 @@
-import { getRecipes } from './recipes.mjs';
+import recipes from './recipes.mjs'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const recipeList = document.getElementById('recipe-list');
-  const recipes = getRecipes();
+  const recipeList = document.getElementById('recipe-list')
 
   recipes.forEach(recipe => {
-    const recipeCard = document.createElement('article');
+    const recipeCard = document.createElement('article')
+    recipeCard.classList.add('recipe-card')
     recipeCard.innerHTML = `
       <h2>${recipe.name}</h2>
-      <img src="images/${recipe.image}" alt="${recipe.name}">
+      <img src="${recipe.image}" alt="${recipe.name}">
       <p>${recipe.description}</p>
-    `;
-    recipeList.appendChild(recipeCard);
-  });
-});
+    `
+    recipeList.appendChild(recipeCard)
+  })
+})
